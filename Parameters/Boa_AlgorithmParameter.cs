27bf1a96fa632ec587d4841f8b05c295f1d14004
vflsva <71788361+vflsva@users.noreply.github.com>
2020-10-12 20:16:06 -0400

    Add files via upload

---
+++ Parameters/Boa_AlgorithmParameter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Boa
+{
+    public class Boa_AlgorithmParameter : GH_Param<Boa_Algorithm>
+    {
+        public Boa_AlgorithmParameter()
+          : base("Boa Algorithm", "Algo",
+            "Parameter that stores a Boa Algorithm.",
+            "Params", "Primitive", GH_ParamAccess.item)
+        {
+            //data
+        }
+
+        protected override System.Drawing.Bitmap Icon => Boa2.Properties.Resource1.algorithmIcon;// Properties.Resources.AlgorithmIcon;
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("f1b6ca48-e911-4bb9-9004-fbc43a88d77d"); }
+        }
+    }
+}
